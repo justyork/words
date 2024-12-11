24a6490d28f887014050910bbabcad1e829450ce
york <yorkshp@gmail.com>
2024-12-11 08:16:23 +0200

    Add dotenv support for database configuration and include new dependencies

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('APP_DEBUG'));
