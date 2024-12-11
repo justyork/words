7259dc0cc93d99d4e40d90b567af5f7ac2ef5bbb
york <yorkshp@gmail.com>
2024-12-11 08:00:36 +0200

    Add env

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=home',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
